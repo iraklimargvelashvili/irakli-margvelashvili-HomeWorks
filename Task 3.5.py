382b70a311f21298ea7f0368b9345d6469051732
iraklimargvelashvili <90346219+iraklimargvelashvili@users.noreply.github.com>
2024-09-19 22:00:41 +0400

    Update Task 3.5.py

--- Task 3.5.py
+++ Task 3.5.py
@@ -1,5 +1,24 @@
-import datetime
-import forex_python
+from datetime import date
+import pycoingecko
 
-date = input("Write the date, when you bought the bitcoin (year,month,day): ")
-price = input("Write down the value of Bitcoin: ")
+curr_date = str(date.today())
+curr_date = curr_date[8:] + "-" + curr_date[5:7] + "-" + curr_date[0:4]
+
+# აქ შეგიძლიათ შეამოწმოთ, მხოლოდ ბოლო 365 დღის განმავლობაში შეძენილ ბიტკოინის ფასსა 
+# და მიმდინარე ფასს შორის განსხვავება
+
+date_str = input("Write the date (example:'dd-mm-yyyy'), when you bought the bitcoin: ")
+
+# Initialize CoinGecko API client
+coinGecko = pycoingecko.CoinGeckoAPI()
+
+btc_data = coinGecko.get_coin_history_by_id(id='bitcoin', date=date_str)
+price = round(btc_data['market_data']['current_price']['usd'])
+
+btc_data_current = coinGecko.get_coin_history_by_id(id='bitcoin', date=curr_date)
+current_price = round(btc_data_current['market_data']['current_price']['usd'])
+
+if(price > current_price):
+    print("You lost",price - current_price,"$")
+else:
+    print("You won",current_price - price,"$")
