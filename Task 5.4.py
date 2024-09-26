9f1d3417f34b674c4e5fa1347645b05cbb634e73
iraklimargvelashvili <90346219+iraklimargvelashvili@users.noreply.github.com>
2024-09-26 13:25:21 +0400

    Add files via upload

---
+++ Task 5.4.py
@@ -0,0 +1,23 @@
+# ნაძვის ხის სიგრძეში * არ შემიყვანია, ის ვარსკვლავად ჩავთვალე
+hight = int(input("Write Christmas tree hight: "))
+christ_tree = ""
+
+if(hight <= 0 or hight >= 50):
+    print("Please, write number more, than 0 and less, than 50")
+else:
+    empty_space_for_star = ""
+    tree = "*"
+    for _ in range(hight+2):
+        empty_space_for_star += ' ' 
+    tree = f"{empty_space_for_star}{tree}{empty_space_for_star}\n"
+    for grow in range(1,hight+1):
+        empty_space = ""
+        floor = ""
+        sticks = "|"
+        for _ in range(grow):
+            sticks += '||'
+        for _ in range(hight+1 - grow):
+            empty_space += ' '
+        floor = f"{empty_space}/{sticks}\\{empty_space}\n"
+        tree += floor
+    print(tree)
